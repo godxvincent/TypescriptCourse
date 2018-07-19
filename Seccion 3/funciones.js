861598d42e07a4b9b91d6d3c7b8e9cf7b29b49a1
godxvincent <godxvincent@gmail.com>
2018-07-18 23:09:59 -0500

    Section Four Added - Objects and customized types in Typescript

--- Seccion 3/funciones.js
+++ Seccion 3/funciones.js
@@ -1,1 +1,73 @@
 "use strict";
+// Definición del tipo de datos del retorno.
+var heroe2 = "Flash";
+function imprime_heroe() {
+    return heroe2;
+}
+console.log(imprime_heroe());
+// Definición de parametros obligatorios.
+function nombreCompleto(nombre, apellido) {
+    return nombre + " " + apellido;
+}
+var nombreP1 = nombreCompleto("Ricardo", "Vargas");
+console.log(nombreP1);
+// Parametros opcionales -> En javascript son opcionales
+function nombreCompletoDos(nombre, apellido) {
+    if (apellido) {
+        return nombre + " " + apellido;
+    }
+    else {
+        return "" + nombre;
+    }
+}
+var nombreP2 = nombreCompletoDos("Ricardo");
+console.log(nombreP2);
+// Parametros por defecto -> En javascript son opcionales, sin embargo no se puede hacer
+// en conjunto con los parametros opcionales.
+function nombreCompletoTres(nombre, apellido, capitalizado) {
+    if (capitalizado === void 0) { capitalizado = true; }
+    if (capitalizado) {
+        return capitalizar(nombre) + " " + capitalizar(apellido);
+    }
+    else {
+        return nombre + " " + apellido;
+    }
+}
+function capitalizar(texto) {
+    return texto.charAt(0).toUpperCase() + texto.substr(1).toLowerCase();
+}
+var nombreP3 = nombreCompletoTres("paula", "martinez");
+console.log(nombreP3);
+// PArametros rest, funcionan igual que los parametros en ECMAScript6
+function nombreCompletosCuatro(nombre) {
+    var otrosParametros = [];
+    for (var _i = 1; _i < arguments.length; _i++) {
+        otrosParametros[_i - 1] = arguments[_i];
+    }
+    return nombre + " " + otrosParametros.join(" ");
+}
+var nombreP4 = nombreCompletosCuatro("Ricardo", "Andres", "Vargas", "Martinez");
+console.log(nombreP4);
+// Tipo funcion, es una forma de declarar una variable de tipo funciónamespace
+// indicando el tipo de datos de entrada y los retornos.
+function suma(a, b) {
+    return a + b;
+}
+function saludar(nombre) {
+    return "I'm " + nombre;
+}
+function imprimir() {
+    console.log("Esto es un mensaje");
+}
+var miFuncion;
+miFuncion = 10;
+// PAra controlar la priemra función se hace
+// let miFuncion : (x:number, y:number) => number
+miFuncion = suma;
+console.log(miFuncion(5, 5));
+// let miFuncion : (x:string) => string
+miFuncion = saludar;
+console.log(miFuncion("Ricardo"));
+// let miFuncion : () => void
+miFuncion = imprimir;
+miFuncion();
---
+++ Seccion 3/funciones.ts
@@ -0,0 +1,79 @@
+// Definición del tipo de datos del retorno.
+let heroe2:string = "Flash";
+
+function imprime_heroe():string {
+    return heroe2;
+}
+
+console.log(imprime_heroe());
+
+
+// Definición de parametros obligatorios.
+function nombreCompleto (nombre:string, apellido:string): string {
+    return `${nombre} ${apellido}`;
+}
+
+let nombreP1:string = nombreCompleto("Ricardo", "Vargas");
+console.log(nombreP1);
+
+// Parametros opcionales -> En javascript son opcionales
+function nombreCompletoDos (nombre:string, apellido?:string): string {
+    if (apellido) {
+        return `${nombre} ${apellido}`;
+    } else {
+        return `${nombre}`;
+    }
+}
+let nombreP2:string = nombreCompletoDos("Ricardo");
+console.log(nombreP2);
+
+// Parametros por defecto -> En javascript son opcionales, sin embargo no se puede hacer
+// en conjunto con los parametros opcionales.
+function nombreCompletoTres (nombre:string, apellido:string,
+                            capitalizado?:boolean = true): string {
+    if (capitalizado) {
+        return `${capitalizar(nombre)} ${capitalizar(apellido)}`;
+    } else {
+        return `${nombre} ${apellido}`;
+    }
+}
+
+function capitalizar( texto:string ):string {
+    return texto.charAt(0).toUpperCase() + texto.substr(1).toLowerCase();
+}
+
+let nombreP3:string = nombreCompletoTres("paula","martinez");
+console.log(nombreP3);
+
+// PArametros rest, funcionan igual que los parametros en ECMAScript6
+function nombreCompletosCuatro(nombre:string, ...otrosParametros:string[]) {
+    return nombre + " " + otrosParametros.join(" ");
+}
+let nombreP4:string = nombreCompletosCuatro("Ricardo","Andres","Vargas","Martinez");
+console.log(nombreP4);
+
+
+// Tipo funcion, es una forma de declarar una variable de tipo funciónamespace
+// indicando el tipo de datos de entrada y los retornos.
+function suma(a:number, b:number): number {
+    return a + b;
+}
+function saludar(nombre:string) : string {
+    return "I'm " + nombre;
+}
+function imprimir() {
+    console.log("Esto es un mensaje");
+}
+
+let miFuncion : (x:number, y:number) => number;
+miFuncion = 10;
+// PAra controlar la priemra función se hace
+// let miFuncion : (x:number, y:number) => number
+miFuncion = suma;
+console.log(miFuncion(5,5));
+// let miFuncion : (x:string) => string
+miFuncion = saludar;
+console.log(miFuncion("Ricardo"));
+// let miFuncion : () => void
+miFuncion = imprimir;
+miFuncion();
