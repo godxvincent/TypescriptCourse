3005f93997f21ca5912f99d5e859cd8876092f18
godxvincent <godxvincent@gmail.com>
2018-07-20 20:32:14 -0500

    Section Six finished, it was just a recall for new features in ES6 and a little practice about it

---
+++ Seccion 6/app.js
@@ -0,0 +1,38 @@
+
+// Convierta este archivo de ES5 a ES6 - JavaScript 2015
+// OJO: No es a TypeScript, solo JAVASCRIPT
+
+
+// ==============================
+//  Spiderman info
+// ==============================
+// Constantes?
+const HEROE = "Spiderman";
+
+// Declaracion de variables?
+// let spiderman = "Peter Parker",
+//     venom = "Eddie Brock";
+
+// Destructuracion de arreglos?
+let versiones = ["Spider-Man 2099","Spider-Girl","Ultimate Spider-Man"];
+
+let [spiderman2099,spidergirl,ultimate] = versiones;
+
+// Destructuracion de objetos?
+let villanos = {
+  venom: "Eddie Brock",
+  carnage: "Cletus Kasady",
+  sandman: "William Baker"
+};
+
+let {venom,carnage,sandman} = villanos;
+
+// Ciclo for Of?
+for(var i=0; i<= versiones.length - 1; i++){
+  var spider = versiones[i];
+  console.log(spider);
+}
+for ( version of versiones ) {
+    console.log( version  );
+
+}
